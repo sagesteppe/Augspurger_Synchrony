38c34835051a4154f8ae0cb3e6ee73debf97898c
reedbenkendorf27@gmail.com <steppe>
2022-03-19 22:25:36 -0500

    working on map2, have decided to calculate the self temporal intersect to simply subtract from a two column comparision

--- scripts/augspurger_sync.R
+++ scripts/augspurger_sync.R
@@ -4,8 +4,16 @@ library(lubridate)
 
 
 filtered_clean <- read_csv(paste0(here(), "/data/filtered_clean.csv"))
+dummy_dat <- read_csv(paste0(here(), "/data/dummy_data.csv"))
 
-augs_synchrony_2012 <- function(dataset, frst_day, lst_day, year_samp, grp_var1){
+
+syn_prep<-filtered_clean %>% 
+  group_by(individual, year, species, nest_duration, nest_start, nest_end, repro_output) %>% 
+  distinct(individual) %>% 
+  ungroup() 
+
+
+augs_synchrony <- function(dataset, frst_day, lst_day, year_samp, grp_var1){
   
   # Dataset a data frame containing values to perform calculations on 
   # year_samp = column holding information on year, 
@@ -26,14 +34,15 @@ augs_synchrony_2012 <- function(dataset, frst_day, lst_day, year_samp, grp_var1)
     
     # TERM A FOR THE INDIVIDUAL
     
-    # term one population - 1 under 1.
+    # term one population 
     mutate(at1 = 1/(n() -1)) %>% 
     mutate(across(c(!!frst_day, !!lst_day),
                   function(x) as_date(x, origin = paste0(year-1, "-12-31")))
            ) %>% 
-    
     mutate(interval_obs = interval(ymd(!!frst_day), ymd(!!lst_day))) %>%
     
+    # term two, duration of event for individual. note duration returns seconds, 
+    # these data are at resolution of day, hence we convert
     mutate(at2 = 1/
              (as.numeric(
         lubridate::as.duration(!!lst_day - !!frst_day)
@@ -42,51 +51,25 @@ augs_synchrony_2012 <- function(dataset, frst_day, lst_day, year_samp, grp_var1)
         )
       ) %>% 
     
-    # term two, duration of event for individual. note duration returns seconds, 
-    # these data are at resolution of day, hence we convert,
-    # if you are using high temporal resolution 
-    # change the division constant (86400) at the end.
-    
-    mutate(at3 = map(interval_obs, ~ sum(as.duration(intersect(as.numeric(.x), interval_obs) %>% 
+    # term 3 
+    mutate(at3 = map(interval_obs, ~ sum(as.duration(lubridate::intersect(as.numeric(.x), interval_obs) %>% 
                                                        as.numeric(replace_na(0)/86400))))) %>%
     mutate(at3 = as.numeric(at3)/86400) %>% 
     
   # term three the sum of overlap between all individuals in pop.
+    rowwise() %>% 
     mutate(augs.indx.indiv. = at1 * at2 * at3) %>%
+    group_by(!!year_samp, !!grp_var1) %>%
     
     ## TERM B FOR THE POPULATION
     mutate(bt1 = 1/n()) %>%
     mutate(bt2 = sum(augs.indx.indiv.)) %>%
-    mutate(augs.index.pop = bt1 * bt2)  %>%
-    dplyr::select(-at1, -at2, -at3, -bt1, -bt2, -interval_obs)
+    mutate(augs.index.pop = bt1 * bt2)  #%>%
+#    dplyr::select(-at1, -at2, -at3, -bt1, -bt2, -interval_obs)
   
   return(results)
   
 }
-
-
-
-##################################### Running it
-
-
-#  This code chunk was primarily used to change column names to match the function and filter out not relevant columns
-
- syn_prep<-filtered_clean %>% 
-   group_by(individual, year, species, nest_duration, nest_start, nest_end, repro_output) %>% 
-   distinct(individual) %>% 
-   ungroup() 
-   
-   # SHOULD BE ACCOMODATED BY LISTED NAMES
-   rename("frst_day"="nest_start", "lst_day"="nest_end", "year_samp"="year") #These names match the names in the function
-
-
-# A function was created for each year (there were augs_synchrony_2012, 2013, etc) and I filtered the dataframe by the relevant year using the hashed out chunk below and then merged all years together after I ran the functions. 
-
-syn_prep <- syn_prep %>% 
-   mutate(across(where(is.numeric), function(x) as.integer(x))) %>% 
-   na.omit() 
-
-
 
 testing <- augs_synchrony_2012(dataset = syn_prep, 
                     frst_day = nest_start, 
@@ -95,5 +78,75 @@ testing <- augs_synchrony_2012(dataset = syn_prep,
                     grp_var1 = species
                     )
 
+testing_dum <- augs_synchrony(dataset = dummy_dat, 
+                               frst_day = flower_start, 
+                               lst_day = flower_end,
+                               year_samp = year,
+                               grp_var1 = species
+)
 
-example <- 
+
+
+dummy_dat <- dummy_dat %>% filter(species == 'tauschia') # the first row of this data frame should 
+# have 201 intersected days (not 301)
+
+
+abet <- dummy_dat %>% 
+  mutate(at1 = 1/(n() -1)) %>% 
+  mutate(across(c(flower_start, flower_end),
+                function(x) as_date(x, origin = paste0(year-1, "-12-31")))
+  ) %>% 
+  mutate(interval_obs = interval(ymd(flower_start), ymd(flower_end))) %>%
+  
+  # term two, duration of event for individual. note duration returns seconds, 
+  # these data are at resolution of day, hence we convert
+  mutate(at2 = 1/
+           (as.numeric(
+             lubridate::as.duration(flower_end - flower_start)
+           )
+           /86400
+           )
+  )# %>% 
+
+
+abet1 <- abet %>% 
+  mutate(at3 = map(interval_obs, ~ sum(lubridate::intersect(as.numeric(.x), interval_obs) %>% 
+                                                   as.numeric(replace_na(0)/86400)))) %>%
+  mutate(at3 = as.numeric(at3)/86400) 
+
+
+targets <- abet %>% select(interval_obs)
+contrasts <- abet %>% select(interval_obs)
+third_term$interval_obs <- as.numeric(third_term$interval_obs)
+
+mutate(to_subtract = intersect())
+
+abet2 <- abet1 %>% 
+  mutate(interval_obs2 = interval_obs) %>% 
+  rowwise() %>% 
+  mutate(to_subtract = as.numeric(intersect(interval_obs, interval_obs2))) %>% 
+  ungroup() %>% 
+  map2(.x = interval_obs, .y = interval_obs2, 
+       .f = ~{results = sum(as.numeric(intersect(.x$interval_obs, .y$interval_obs2)))})
+
+
+AS_overlap <- function(x){
+  
+  identified <- x %>% mutate(observation_id = 1:nrow(.))
+  contrasts <- identified %>% dplyr::select(interval_obs, observation_id)
+  
+  targets <- contrasts
+  third_term <- targets
+  third_term$interval_obs <- ""
+  
+  for (i in nrow(targets)){
+    third_term[i,] <- sum(lubridate::intersect(i, contrasts[-i,]))
+  }
+  return(third_term)
+}
+
+out <- AS_overlap(abet)
+
+a <- lubridate::intersect(targets[i,], contrasts)
+lubridate::intersect(targets[1,1], contrasts[2,1])
+
