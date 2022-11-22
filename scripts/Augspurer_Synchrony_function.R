3804a733f7bc4f6da828a4e02871ef9e0d99a3aa
sagesteppe <reedbenkendorf27@gmail.com>
2022-11-21 18:09:00 -0700

    variable number of groups and packages names added

--- scripts/Augspurer_Synchrony_function.R
+++ scripts/Augspurer_Synchrony_function.R
@@ -1,35 +1,37 @@
-augs_synchrony <- function(dataset, frst_day, lst_day, year_samp, grp_var1){
+#' Calculcate augspurgers index of synchrony
+#' 
+#' Calculcate Augspurgers Index of Synchrony from a dataframe. 
+#' @param dataset a data frame containing values to perform calculations on 
+#' @param year_samp = column holding information on year, required even if all events occur in
+#' the same year due to leap years. 
+#' @param frst_day = column containing the INITIATION DATE of a phenological event
+#' @param lst_dat = column containing the FINAL DATE of a phenological event
+#' @param ... in this case grouping variables (within year!)
+#' @example 
+#' @export
+augs_synchrony <- function(dataset, frst_day, lst_day, year_samp, ...){
   
-  # Dataset a data frame containing values to perform calculations on 
-  # year_samp = column holding information on year, 
-  # frst_day = column containing the INITIATION DATE of a phenological event
-  # lst_dat = column containing the FINAL DATE of a phenological event
-  # grp_var1 = one level of grouping to have the function focus on. 
-  
-  frst_day <- enquo(frst_day)
-  lst_day <- enquo(lst_day)
-  
-  # grouping variables too
-  year_samp <- enquo(year_samp)
-  grp_var1 <- enquo(grp_var1)
-  #grp_var2 <- enquo(grp_var2)
+  frst_day <- rlang::enquo(frst_day)
+  lst_day <- rlang::enquo(lst_day)
+  year_samp <- rlang::enquo(year_samp)
   
   dataset <- dataset %>% 
-    mutate(across(c(!!frst_day, !!lst_day), function(x) as.integer(x)))  
+    dplyr::mutate(across(c(!!frst_day, !!lst_day), function(x) as.integer(x)))  
   
   results <- dataset %>% 
-    group_by(!!year_samp, !!grp_var1, 
+    dplyr::group_by(!!year_samp, !!!rlang::ensyms(...), 
              #!!grp_var2
     ) %>% # the year of analysis, and the population variables if on multiyear data.
     
     # TERM A FOR THE INDIVIDUAL $X_{i}$
     
     # term one population (\frac{1}{n-1})
-    mutate(at1 = 1/(n() -1)) %>% 
-    mutate(across(c(!!frst_day, !!lst_day),
-                  function(x) as_date(x, origin = paste0(year_samp-1, "-12-31")))
+    dplyr::mutate(at1 = 1/(n() -1)) %>% 
+    dplyr::mutate(dplyr::across(c(!!frst_day, !!lst_day),
+                  function(x) lubridate::as_date(x, origin = paste0(year_samp-1, "-12-31")))
     ) %>% 
-    mutate(interval_obs = interval(ymd(!!frst_day), ymd(!!lst_day))) %>%
+    dplyr::mutate(interval_obs = lubridate::interval(lubridate::ymd(!!frst_day), 
+                                                     lubridate::ymd(!!lst_day))) %>%
     
     # term two, (\frac{1}{f_{i}}) duration of event for individual. 
     # note duration returns seconds, but these data are at resolution of day. 
@@ -43,22 +45,22 @@ augs_synchrony <- function(dataset, frst_day, lst_day, year_samp, grp_var1){
     
     # term 3: all days individual i is flowering with other individuals
     # \sum_{j = i}^{n} e_{j != i}
-    mutate(at3 = map_dbl(interval_obs, # @ shs on SO
+    dplyr::mutate(at3 = purrr::map_dbl(interval_obs, # @ shs on SO
                          \(x) x |> 
-                           intersect(interval_obs) |>  
-                           int_length() |> 
-                           sum(na.rm = T) - int_length(x)
+                           lubridate::intersect(interval_obs) |>  
+                           lubridate::int_length() |> 
+                           sum(na.rm = T) - lubridate::int_length(x)
     ) / 86400) %>% 
     
     # the index of synchrony for individual i
-    rowwise() %>% 
+    dplyr::rowwise() %>% 
     mutate(augs.indx.indiv. = at1 * at2 * at3) %>%
-    group_by(!!year_samp, !!grp_var1) %>%
+    dplyr::group_by(!!year_samp, !!!rlang::ensyms(...)) %>%
     
     ## TERM B FOR THE POPULATION
-    mutate(bt1 = 1/n()) %>% # $\frac{1}{n}$ 1/sample size
-    mutate(bt2 = sum(augs.indx.indiv.)) %>% # \sum_{j = 1}^{n}X_{i}
-    mutate(augs.index.pop = bt1 * bt2)  %>% 
+    dplyr::mutate(bt1 = 1/n()) %>% # $\frac{1}{n}$ 1/sample size
+    dplyr::mutate(bt2 = sum(augs.indx.indiv.)) %>% # \sum_{j = 1}^{n}X_{i}
+    dplyr::mutate(augs.index.pop = bt1 * bt2)  %>% 
     
     # clean return data
     dplyr::select(-at1, -at2, -at3, -bt1, -bt2, -interval_obs)
