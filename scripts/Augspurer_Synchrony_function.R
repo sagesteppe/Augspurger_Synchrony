a3af124438ca9913932bb28418dc0794482c3f09
sagesteppe <reedbenkendorf27@gmail.com>
2022-11-22 18:14:07 -0700

    emulated the original plots, swapped up the sample data

--- scripts/Augspurer_Synchrony_function.R
+++ scripts/Augspurer_Synchrony_function.R
@@ -14,33 +14,35 @@ augs_synchrony <- function(dataset, frst_day, lst_day, year_samp, ...){
   frst_day <- rlang::enquo(frst_day)
   lst_day <- rlang::enquo(lst_day)
   year_samp <- rlang::enquo(year_samp)
-  grp_vars <- enquos(!!year_samp, ...)
+  grp_vars <- rlang::enquos(...)
   
-  dataset <- dataset %>% 
+  dataset <- dataset |> 
     dplyr::mutate(across(c(!!frst_day, !!lst_day), function(x) as.integer(x)))  
   
-  results <- dataset %>% 
-    dplyr::group_by( !!!(grp_vars)) %>%
+  results <- dataset |>
+    dplyr::group_by(!!year_samp, !!!(grp_vars)) |> 
     
     # TERM A FOR THE INDIVIDUAL $X_{i}$
     
     # term one population (\frac{1}{n-1})
-    dplyr::mutate(at1 = 1/(n() -1)) %>% 
+    dplyr::mutate(at1 = 1/(dplyr::n() -1)) |>
     dplyr::mutate(dplyr::across(c(!!frst_day, !!lst_day),
-                  function(x) lubridate::as_date(x, origin = paste0(year_samp-1, "-12-31")))
-    ) %>% 
+                  function(x) 
+                    lubridate::as_date(x, origin = 
+                                         paste0(!!year_samp - 1, "-12-31")))
+    ) |> 
     dplyr::mutate(interval_obs = lubridate::interval(lubridate::ymd(!!frst_day), 
-                                                     lubridate::ymd(!!lst_day))) %>%
+                                                     lubridate::ymd(!!lst_day))) |> 
     
     # term two, (\frac{1}{f_{i}}) duration of event for individual. 
     # note duration returns seconds, but these data are at resolution of day. 
-    mutate(at2 = 1/
+    dplyr::mutate(at2 = 1/
              (as.numeric(
                lubridate::as.duration(!!lst_day - !!frst_day)
              )
              /86400
              )
-    ) %>% 
+    ) |> 
     
     # term 3: all days individual i is flowering with other individuals
     # \sum_{j = i}^{n} e_{j != i}
@@ -49,17 +51,17 @@ augs_synchrony <- function(dataset, frst_day, lst_day, year_samp, ...){
                            lubridate::intersect(interval_obs) |>  
                            lubridate::int_length() |> 
                            sum(na.rm = T) - lubridate::int_length(x)
-    ) / 86400) %>% 
+    ) / 86400) |> 
     
     # the index of synchrony for individual i
-    dplyr::rowwise() %>% 
-    dplyr::mutate(augs.indx.indiv. = at1 * at2 * at3) %>%
-    dplyr::group_by(!!!(grp_vars)) %>%
+    dplyr::rowwise() |> 
+    dplyr::mutate(augs.indx.indiv. = at1 * at2 * at3) |> 
+    dplyr::group_by(!!year_samp,  !!!(grp_vars)) |> 
     
     ## TERM B FOR THE POPULATION
-    dplyr::mutate(bt1 = 1/n()) %>% # $\frac{1}{n}$ 1/sample size
-    dplyr::mutate(bt2 = sum(augs.indx.indiv.)) %>% # \sum_{j = 1}^{n}X_{i}
-    dplyr::mutate(augs.index.pop = bt1 * bt2)  %>% 
+    dplyr::mutate(bt1 = 1/dplyr::n()) |>  # $\frac{1}{n}$ 1/sample size
+    dplyr::mutate(bt2 = sum(augs.indx.indiv.)) |>  # \sum_{j = 1}^{n}X_{i}
+    dplyr::mutate(augs.index.pop = bt1 * bt2) |>  
     
     # clean return data
     dplyr::select(-at1, -at2, -at3, -bt1, -bt2, -interval_obs)
--- scripts/augspurger_sync_developement.R
+++ scripts/augspurger_sync_developement.R
@@ -3,12 +3,50 @@ library(tidyverse)
 library(lubridate)
 
 #set_here(path='.')
-dummy_dat <- read_csv(paste0(here(), "/data/dummy_data.csv"))
-
+setwd('/hdd/Assorted_R_functions/Augspurger_Synchrony/scripts')
+dummy_dat <- read.csv("../data/dummy_data.csv")
 
 test_dummy <- augs_synchrony(dataset = dummy_dat,
-                             frst_day = flower_start, 
-                             lst_day = flower_end,
-                             year_samp = year, 
-                             species
-                             )
+                             frst_day = flower_start, lst_day = flower_end,
+                             year_samp = year, species )
+
+
+
+test_dummyp <- test_dummy |>
+  dplyr::group_by(species) |> 
+  dplyr::mutate(ID = 1:dplyr::n(),
+                flower_start_DOY = lubridate::yday(flower_start),
+                flower_end_DOY   = lubridate::yday(flower_end))
+
+library(ggplot2)
+
+ggplot(test_dummyp, aes(y = ID, x = median_flowers)) +
+  geom_point() +
+  facet_wrap('species', scales = 'free', nrow = 1) +
+  theme_bw() +
+  geom_segment(aes(x = flower_start_DOY, y = ID, xend = flower_end_DOY, yend = ID), lty = 3) +
+  geom_segment(aes(x = lower_sd, y = ID, xend = upper_sd, yend = ID),
+               arrow = arrow(length = unit(0.03, "npc"), ends = "both")) +
+  theme(strip.background = element_blank(),
+        axis.line = element_line(colour = "black"),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.border = element_blank(),
+        panel.background = element_blank()) +
+  labs(title = 'Ficticious Floral Synchonry', x = 'DOY', y = 'Plant ID')
+  
+
+ggplot(plot14a, aes(x=nest_start, xend=nest_end, y=rowid, colour=indiv_syn)) +
+  geom_dumbbell(colour_xend = "gray90", colour_x = "gray90", size=1.5, size_x = .5, size_xend = .5) +
+  scale_color_gradient(low="yellow", high="red", limits = c(0,0.4)) +
+  theme_classic()+
+  #xlab("Day of Year")+
+  #ylab("Individual Nest")+
+  theme(plot.title=element_text(hjust=0.5),
+        axis.text.y = element_blank(),
+        axis.title.x = element_blank(),
+        axis.title.y = element_blank(),
+        panel.spacing = unit(1, "lines")) +
+  facet_wrap(~year)+
+  force_panelsizes(cols = unit(c(5, 5), "cm"),
+                   TRUE)
