34c4be255eba0f5afbc4431c8dbb39dcb7acd837
sagesteppe <reedbenkendorf27@gmail.com>
2024-07-21 16:11:36 -0600

    more robust examples of what synchony is

--- scripts/augs_synchrony.R
+++ scripts/augs_synchrony.R
@@ -1,71 +1,80 @@
 #' Calculate augspurgers index of synchrony
-#' 
-#' Calculate Augspurgers Index of Synchrony from a dataframe. 
-#' @param dataset a data frame containing values to perform calculations on 
-#' @param year_samp = column holding information on year, required even if all events occur in
-#' the same year due to leap years. 
-#' @param frst_day = column containing the INITIATION DATE of a phenological event
-#' @param lst_dat = column containing the FINAL DATE of a phenological event
+#'
+#' Calculate Augspurgers Index of Synchrony from a dataframe.
+#'
+#' @param dataset a data frame containing values to perform calculations on
+#' @param year_samp column holding information on year, required even if all events occur in the same year due to leap years
+#' @param frst_day column containing the INITIATION DATE of a phenological event
+#' @param lst_day column containing the FINAL DATE of a phenological event
 #' @param ... in this case grouping variables (within year!)
-#' @example 
+#' @return a dataframe with attached results, see example
+#' @examples
+#' library(AugspurgerIndex)
+#' flowering_data <- data(flowering_data)
+#' head(flowering_data)
+#' synchrony <- augs_synchrony(dataset = flowering_data,
+#'   frst_day = flower_start, lst_day = flower_end,
+#'   year_samp = year, species )
+#'
+#' head(synchrony)
 #' @export
 augs_synchrony <- function(dataset, frst_day, lst_day, year_samp, ...){
-  
+
   frst_day <- rlang::enquo(frst_day)
   lst_day <- rlang::enquo(lst_day)
   year_samp <- rlang::enquo(year_samp)
   grp_vars <- rlang::enquos(...)
-  
-  dataset <- dataset |> 
-    dplyr::mutate(across(c(!!frst_day, !!lst_day), function(x) as.integer(x)))  
-  
+
+  dataset <- dataset |>
+    dplyr::mutate(across(c(!!frst_day, !!lst_day), function(x) as.integer(x)))
+
   results <- dataset |>
-    dplyr::group_by(!!year_samp, !!!(grp_vars)) |> 
-    
+    dplyr::group_by(!!year_samp, !!!(grp_vars)) |>
+
     # TERM A FOR THE INDIVIDUAL $X_{i}$
-    
+
     # term one population (\frac{1}{n-1})
     dplyr::mutate(at1 = 1/(dplyr::n() -1)) |>
     dplyr::mutate(dplyr::across(c(!!frst_day, !!lst_day),
-                  function(x) 
-                    lubridate::as_date(x, origin = 
+                  function(x)
+                    lubridate::as_date(x, origin =
                                          paste0(!!year_samp - 1, "-12-31")))
-    ) |> 
-    dplyr::mutate(interval_obs = lubridate::interval(lubridate::ymd(!!frst_day), 
-                                                     lubridate::ymd(!!lst_day))) |> 
-    
-    # term two, (\frac{1}{f_{i}}) duration of event for individual. 
-    # note duration returns seconds, but these data are at resolution of day. 
+    ) |>
+    dplyr::mutate(interval_obs = lubridate::interval(lubridate::ymd(!!frst_day),
+                                                     lubridate::ymd(!!lst_day))) |>
+
+    # term two, (\frac{1}{f_{i}}) duration of event for individual.
+    # note duration returns seconds, but these data are at resolution of day.
     dplyr::mutate(at2 = 1/
              (as.numeric(
                lubridate::as.duration(!!lst_day - !!frst_day)
              )
              /86400
              )
-    ) |> 
-    
+    ) |>
+
     # term 3: all days individual i is flowering with other individuals
     # \sum_{j = i}^{n} e_{j != i}
     dplyr::mutate(at3 = purrr::map_dbl(interval_obs, # @ shs on SO
-                         \(x) x |> 
-                           lubridate::intersect(interval_obs) |>  
-                           lubridate::int_length() |> 
+                         \(x) x |>
+                           lubridate::intersect(interval_obs) |>
+                           lubridate::int_length() |>
                            sum(na.rm = T) - lubridate::int_length(x)
-    ) / 86400) |> 
-    
+    ) / 86400) |>
+
     # the index of synchrony for individual i
-    dplyr::rowwise() |> 
-    dplyr::mutate(augs.indx.indiv. = at1 * at2 * at3) |> 
-    dplyr::group_by(!!year_samp,  !!!(grp_vars)) |> 
-    
+    dplyr::rowwise() |>
+    dplyr::mutate(augs.indx.indiv. = at1 * at2 * at3) |>
+    dplyr::group_by(!!year_samp,  !!!(grp_vars)) |>
+
     ## TERM B FOR THE POPULATION
     dplyr::mutate(bt1 = 1/dplyr::n()) |>  # $\frac{1}{n}$ 1/sample size
     dplyr::mutate(bt2 = sum(augs.indx.indiv.)) |>  # \sum_{j = 1}^{n}X_{i}
-    dplyr::mutate(augs.index.pop = bt1 * bt2) |>  
-    
+    dplyr::mutate(augs.index.pop = bt1 * bt2) |>
+
     # clean return data
     dplyr::select(-at1, -at2, -at3, -bt1, -bt2, -interval_obs)
-  
+
   return(results)
-  
+
 }
--- scripts/augspurger_sync_developement.R
+++ scripts/augspurger_sync_developement.R
@@ -1,19 +1,17 @@
-library(here)
-library(tidyverse)
-library(lubridate)
+setwd('~/Documents/assoRted/Augspurger_Synchrony/data')
+pena_flrs <- read.csv("../data/dummy_data.csv")
+save('pena_flrs', file = '~/Documents/assoRted/AugspurgerIndex/data/flowering_data.RData')
 
-#set_here(path='.')
-setwd('/hdd/Assorted_R_functions/Augspurger_Synchrony/scripts')
-dummy_dat <- read.csv("../data/dummy_data.csv")
 
-test_dummy <- augs_synchrony(dataset = dummy_dat,
+head(pena_flrs)
+pena_synchrony <- augs_synchrony(dataset = pena_flrs,
                              frst_day = flower_start, lst_day = flower_end,
                              year_samp = year, species )
+head(pena_synchrony)
 
 
-
-test_dummyp <- test_dummy |>
-  dplyr::group_by(species) |> 
+test_dummyp <- pena_synchrony |>
+  dplyr::group_by(species) |>
   dplyr::mutate(ID = 1:dplyr::n(),
                 flower_start_DOY = lubridate::yday(flower_start),
                 flower_end_DOY   = lubridate::yday(flower_end))
@@ -23,39 +21,56 @@ library(ggplot2)
 ggplot(test_dummyp, aes(y = ID, x = median_flowers)) +
   geom_point(aes(fill = 'Median')) +
   facet_wrap('species', scales = 'free', nrow = 1) +
-  theme_bw() +
-  geom_segment(aes(x = flower_start_DOY, y = ID, xend = flower_end_DOY, yend = ID,
-                   linetype = 'Range')) +
-  geom_segment(aes(x = lower_sd, y = ID, xend = upper_sd, yend = ID, linetype = 'mean SD'),
+  theme_bw() + 
+  geom_segment(aes(x = flower_start_DOY, y = ID, 
+                   xend = flower_end_DOY, yend = ID,
+                   linetype = 'Range')) + 
+  geom_segment(
+    aes(x = lower_sd, y = ID, xend = upper_sd, yend = ID, linetype = 'mean SD'),
                arrow = arrow(length = unit(0.03, "npc"), ends = "both")) +
-  
+
   theme(strip.background = element_blank(),
         axis.line = element_line(colour = "black"),
         panel.grid.major = element_blank(),
         panel.grid.minor = element_blank(),
         axis.ticks.y = element_blank(),
-        axis.text.y = element_blank(),
         panel.border = element_blank(),
+        plot.caption = element_text(hjust = 0, face = 'italic'),
+        axis.text.x = element_text(angle = 45, hjust = 1),
         panel.background = element_blank()) +
   labs(title = 'Fictitious Floral Synchonry', x = 'Day of Year', y = 'Individual',
-       fill = NULL) +
-  scale_linetype_manual("Durations", 
-                        values=c("Range"= 3, 'mean SD' = 1)) +
-  scale_color_manual(values = c("Median" = 'black'))
-  
-  
+       caption = 'Dashed lines indicate the entire time (range) an event is occurring - the sole value used in the index.\nMedian date is the peak of an event on the plant, such as when 50% of all flowers have been produced.\nThe Standard deviation reflects the uncertainty of the sampling on a subset of the individual.', fill = NULL) +
+  scale_linetype_manual("Phenohase\nDuration",
+                        values=c("Range"= 3, 'mean SD' = 1)) + 
+  scale_color_manual(values = c("Median" = 'black')) + 
+  scale_y_continuous(breaks = scales::breaks_pretty()) + 
+  scale_x_continuous(breaks = scales::breaks_pretty())
 
-ggplot(plot14a, aes(x=nest_start, xend=nest_end, y=rowid, colour=indiv_syn)) +
-  geom_dumbbell(colour_xend = "gray90", colour_x = "gray90", size=1.5, size_x = .5, size_xend = .5) +
-  scale_color_gradient(low="yellow", high="red", limits = c(0,0.4)) +
-  theme_classic()+
-  #xlab("Day of Year")+
-  #ylab("Individual Nest")+
-  theme(plot.title=element_text(hjust=0.5),
-        axis.text.y = element_blank(),
-        axis.title.x = element_blank(),
-        axis.title.y = element_blank(),
-        panel.spacing = unit(1, "lines")) +
-  facet_wrap(~year)+
-  force_panelsizes(cols = unit(c(5, 5), "cm"),
-                   TRUE)
+ggsave(path = '../data', filename = 'AugsPanel.png', height = 7, width = 7, units = 'in')
+
+test_dummyp |>
+  dplyr::select(species, ID, augs.index.pop, augs.indx.indiv.) |>
+  ggplot(aes(y = ID, x = augs.indx.indiv.)) + 
+  geom_point() +  
+  geom_line(aes(x = augs.index.pop, colour="-"),  lty = 2) + 
+  facet_wrap('species', scales = 'free', nrow = 1) + 
+  theme_bw() + 
+  scale_y_continuous(breaks = scales::breaks_pretty()) + 
+  scale_x_continuous(breaks = scales::breaks_pretty()) +
+  xlim(0, 1.01) + 
+  labs(x = 'Individual Synchrony', y = 'Individual', 
+       title = 'Fictitious Synchrony Index', 
+       caption = 'The leftmost panels reflect the total and total lack of overlap for the event range.\nThe third panel shows that ca, 2/3 of individuals are flowering at any one time.\nThe final panel displays a real world scenario, discussed below.') + 
+  
+  theme(strip.background = element_blank(),
+        axis.line = element_line(colour = "black"),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(), 
+        axis.ticks.y = element_blank(),
+        panel.border = element_blank(),
+        plot.caption = element_text(hjust = 0, face = 'italic'),
+        axis.text.x = element_text(angle = 45, hjust = 1),
+        panel.background = element_blank())  + 
+  scale_color_manual("Population\nSynchrony", values = c('-' = 'grey50')) 
+  
+ggsave(path = '../data', filename = 'IndexPanel.png', height = 7, width = 7, units = 'in')
